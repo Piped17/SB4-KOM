4bf836d5b87aaa0f478d03708b70eb6403e1f188
Pierre Pedersen <piped17@student.sdu.dk>
2023-05-27 00:42:05 +0200

    Collision detection added via CollisionPlugin class and module, BUG: NUllpointerexception line 22 CollisionPlugin

--- AsteroidsGame/Collision/src/main/java/dk/sdu/piped17/cbse/collisionsystem/CollisionPlugin.java
+++ AsteroidsGame/Collision/src/main/java/dk/sdu/piped17/cbse/collisionsystem/CollisionPlugin.java
@@ -8,36 +8,48 @@ import dk.sdu.piped17.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.piped17.cbse.common.services.IPostEntityProcessingService;
 
 public class CollisionPlugin implements IPostEntityProcessingService {
+
     @Override
     public void process(GameData gameData, World world) {
-        for (Entity hitterEntity : world.getEntities()) {
-            for (Entity collidedEntity : world.getEntities()) {
-                if (hitterEntity.getID().equals(collidedEntity.getID())) {
+        // two for loops for all entities in the world
+        for (Entity entity : world.getEntities()) {
+            for (Entity collisionDetection : world.getEntities()) {
+                // get life parts on all entities
+                LifePart entityLife = entity.getPart(LifePart.class);
+
+                // if the two entities are identical, skip the iteration
+                if (entity.getID().equals(collisionDetection.getID())) {
                     continue;
+
+                    // remove entities with zero in expiration
                 }
 
-                LifePart hitterEntityLifePart = hitterEntity.getPart(LifePart.class);
-                LifePart collidedEntityLifePart = hitterEntity.getPart(LifePart.class);
-
-                if (hitterEntityLifePart.getLife() > 0 && this.collides(hitterEntity, collidedEntity)) {
-                    hitterEntityLifePart.setIsHit(true);
+                // CollisionDetection
+                if (this.collides(entity, collisionDetection)) {
+                    // if entity has been hit, and should have its life reduced
+                    if (entityLife.getLife() > 0) {
+                        entityLife.setLife(entityLife.getLife() - 1);
+                        entityLife.setIsHit(true);
+                        // if entity is out of life - remove
+                        if (entityLife.getLife() <= 0) {
+                            world.removeEntity(entity);
+                        }
+                    }
                 }
             }
         }
     }
 
-    private boolean collides(Entity hitterEntity, Entity collidedEntity) {
-        // Get data for collision detection
-        PositionPart hitterPositionPart = hitterEntity.getPart(PositionPart.class);
-        PositionPart collidedPositionPart = collidedEntity.getPart(PositionPart.class);
-
-        // Calculate distance between
-        float dx =  (hitterPositionPart.getX() - collidedPositionPart.getX());
-        float dy =  (hitterPositionPart.getY() - collidedPositionPart.getY());
-        float distanceBetween = (float) Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-
-        // Check if distance is less than the two radiuses, meaning that they are hitting each other
-        float collisionDistance = hitterEntity.getRadius() + collidedEntity.getRadius();
-        return distanceBetween < collisionDistance;
+    public Boolean collides(Entity entity, Entity entity2) {
+        PositionPart entMov = entity.getPart(PositionPart.class);
+        PositionPart entMov2 = entity2.getPart(PositionPart.class);
+        float dx = (float) entMov.getX() - (float) entMov2.getX();
+        float dy = (float) entMov.getY() - (float) entMov2.getY();
+        float distance = (float) Math.sqrt(dx * dx + dy * dy);
+        if (distance < (entity.getRadius() + entity2.getRadius())) {
+            return true;
+        }
+        return false;
     }
+
 }
